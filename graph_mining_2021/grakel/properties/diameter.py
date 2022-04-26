6298b0aad831f71ce41aa2f1d606a23f041c65c4
Serena Sian Yuan <ssy248@nyu.edu>
2022-04-26 16:33:42 -0700

    Create diameter.py

---
+++ graph_mining_2021/grakel/properties/diameter.py
@@ -0,0 +1,7 @@
+# A study of visibility graphs for time series representations
+
+# properties: diameter d: max len of all shortest paths found
+# btwn any pairs of nodes in (subgraph G' or graph G)
+
+def diameter(g_shortest_paths_mat):
+    return max(g_shortest_paths_mat)
