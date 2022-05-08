46fd0c610fc81e49ef604b61d2654a2e86b8b1dd
Serena Sian Yuan <ssy248@nyu.edu>
2022-05-08 15:27:44 -0700

    Update weighted_random_walks.py

--- graph_mining_2021/grakel/weighted_random_walks.py
+++ graph_mining_2021/grakel/weighted_random_walks.py
@@ -30,4 +30,25 @@ def preferential_navigation(A, q, beta):
     x_ind +=1
   return(new_adj_mat)
     
-      
+# stationary distribution
+
+# pref navigation random walk
+def preferential_navigation_stationary_i(A, q, beta,i):
+    n = len(A)
+    new_adj_mat = np.zeros((n,n))
+    x_ind = 0
+    numerator = 0
+    for ell in range(n):
+        numerator = numerator+ np.pow(q[i]*q[ell], beta)*A[i,ell]
+    denominator =0 
+    for ell in range(n):
+        for m in range(n):
+            denominator= denominator+ np.pow(q[ell]*q[m], beta)*A[ell, m]
+    return (numerator)/(denominator)
+
+
+# max entropy random walks
+
+def max_entropy_random_walk_i(eps, i):
+    
+    return np.pow(eps[i], 2)
